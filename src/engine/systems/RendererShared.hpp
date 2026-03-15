1708c7780d3d5b299602b2e5ff8879359559fd4d
levimc1 <plevimc7@gmail.com>
2026-03-15 21:46:15 +0100

    Window title

--- src/engine/systems/RendererShared.hpp
+++ src/engine/systems/RendererShared.hpp
@@ -21,12 +21,13 @@ namespace engine::internal {
     void cleanup() override;
 
     // Közös state változók
-    inline static GLFWwindow* window;
+    GLFWwindow* window;
 
     // AI mer nincs idegem ezt megírni újra.
     static GLuint loadShader(const std::string& verRelPath, const std::string& fragRelPath);
     inline bool condition() {return !glfwWindowShouldClose(window);}
-
+    
+    // Ehez nem kell instance
     static inline float vertices[] = {
       -0.5f, -0.5f, 0.0f, // Alsó bal
        0.5f, -0.5f, 0.0f, // Alsó jobb
@@ -38,6 +39,30 @@ namespace engine::internal {
       0, 1, 2,
       2, 3, 0,
     };
+
+    //nagyon internal
+    std::string windowTitle = "engine::renderer::windowTitle(name)"; // kezdő ha van
   };
+  
+
+}
+
+// Módosítások pld új ablak, stb.
+namespace engine::renderer {
+  
+  // setup előtt.
+  inline void windowTitle(const std::string& name) {
+    internal::RendererShared& renderer = internal::EngineState::getShared<internal::RendererShared>();
+    if (!renderer.window) {renderer.windowTitle = name;} else {
+      glfwSetWindowTitle(renderer.window, name.c_str());
+    }
+  }
+
+  inline std::string getWindowTitle() {
+    internal::RendererShared& renderer = internal::EngineState::getShared<internal::RendererShared>();
+    if (!renderer.window) {return renderer.windowTitle;} else {
+      return glfwGetWindowTitle(renderer.window);
+    }
+  }
 
 }
