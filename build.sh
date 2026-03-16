f1fa0fc30809c5f815d77e1ee0c28c5eee718752
Porkoláb Levente <porkolabl_2025@wesselenyi-szk1.hu>
2026-03-16 14:13:11 +0100

    build izék

--- build.sh
+++ build.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
-# Build script a C játékprojekthez
-
-# hibán kilép
 set -e
 
-# Build létrehozása ha nincs
+BUILD_TYPE=${1:-Debug}
+
 rm -rf build
 mkdir -p build
 
-# Cmake és build
-cmake -S . -B build -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+cmake -S . -B build -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 cmake --build build
 
-echo "[B] építés befejezve, futtatható build/Game alatt."
+echo "[B] építés befejezve ($BUILD_TYPE)"
 echo "[B] futtatható futtatása"
 
-# futi 
-./build/Game
+./build/Game.exe
